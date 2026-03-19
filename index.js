31083671449ec8f5bab3e0fe9b757ec98bf9f820
Crystal O'Rourke <cforourk@umich.edu>
2026-03-18 22:13:34 -0400

    vault is secure

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //variables for securing vault//
-let code1 = 7 + 3
+let code1
+code1 = 7 + 3
 let code2 = 20 / 2 * 5 - 10
 let code3 = 2 % 2 + 39
-let scooby= "The vault has been secured. The combination is: "
-console.log(`${scooby} ${code1} - ${code2} - ${code3}`);
+let codeA = "code1 + code2 + code3"
+let codeB =   `${code1}-${code2}-${code3}`;
+let message= "The vault has been secured. The combination is: "
+console.log(message, codeA, codeB)
