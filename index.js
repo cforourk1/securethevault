920a5495233beb6c4ab9928a0722b86e2e3d6e65
Crystal O'Rourke <cforourk@umich.edu>
2026-03-18 21:36:15 -0400

    complete

---
+++ index.js
@@ -0,0 +1,6 @@
+//variables for securing vault//
+let code1 = 7 + 3
+let code2 = 20 / 2 * 5 - 10
+let code3 = 2 % 2 + 39
+let scooby= "The vault has been secured. The combination is: "
+console.log(`${scooby} ${code1} - ${code2} - ${code3}`);
